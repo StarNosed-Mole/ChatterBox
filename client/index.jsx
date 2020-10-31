2584c6834550410ad6278a4508a6111c60d794e1
Janis-H <janis_h.aguilar@yahoo.com>
2020-10-31 11:25:53 -0700

    added websocket connenction to frontend and backend

--- client/index.jsx
+++ client/index.jsx
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './styles/index.css';
 
-const App = () => (
-  <p>helloooooo</p>
-);
+const App = () => {
+  const connection = new WebSocket('ws://localhost:4040');
+  const [data, setData] = useState('');
+  connection.onopen = (event) => {
+    console.log('WebSocket is open now.');
+  };
+  connection.onclose = (event) => {
+    console.log('WebSocket is closed now.');
+  };
+  connection.onerror = (event) => {
+    console.log('WebSocket has had an error: ', event);
+  };
+  connection.onmessage = (event) => {
+    // when message is received from server
+    // append to dom
+    setData((prevData) => {
+      let newData = prevData;
+      newData += event.data;
+      return newData;
+    });
+  };
+  return (
+    <>
+      <p id="chat">Data: { data }</p>
+      <form>
+        <input type="text" />
+        <button type="submit" onClick={connection.send()}>Submit</button>
+      </form>
+    </>
+  );
+};
 
 render(<App />, document.getElementById('root'));
